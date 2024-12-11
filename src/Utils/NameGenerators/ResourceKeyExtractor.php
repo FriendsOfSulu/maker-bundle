ccf96eb9d7c718562828461cb6a7cc9a52639174
mamazu <14860264+mamazu@users.noreply.github.com>
2024-12-11 17:30:24 +0100

    Better ResoureExtractor error messages

--- src/Utils/NameGenerators/ResourceKeyExtractor.php
+++ src/Utils/NameGenerators/ResourceKeyExtractor.php
@@ -7,18 +7,30 @@ use Webmozart\Assert\Assert;
 
 class ResourceKeyExtractor implements UniqueNameGenerator
 {
+    private const string RESOURCE_KEY_CONSTANT = "RESOURCE_KEY";
+
     /** @param class-string $className */
     public function getUniqueName(string $className): string
     {
         $reflection = new ReflectionClass($className);
 
-        $resourceKey = $reflection->getConstant('RESOURCE_KEY');
-        if (is_string($resourceKey)) {
-            return $resourceKey;
+        $resourceKey = null;
+        if ($reflection->hasConstant(self::RESOURCE_KEY_CONSTANT)) {
+            $resourceKey = $reflection->getConstant(self::RESOURCE_KEY_CONSTANT);
         }
 
-        $resourceKey = $reflection->getProperty('RESOURCE_KEY')->getValue();
-        Assert::string($resourceKey, 'Resource key must be a "string" but got "'. get_debug_type($resourceKey). '" given');
+        if ($reflection->hasProperty(self::RESOURCE_KEY_CONSTANT)) {
+            $resourceKey = $reflection->getProperty(self::RESOURCE_KEY_CONSTANT)->getValue();
+        }
+
+        Assert::notNull(
+            $resourceKey,
+            'Could not find resource key. It has to be a constant or a property on the class: '.$className,
+        );
+        Assert::string(
+            $resourceKey,
+            'Resource key must be a "string" but got "'. get_debug_type($resourceKey). '" given',
+        );
 
         return $resourceKey;
     }
